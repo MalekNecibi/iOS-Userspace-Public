4c0521a42de40d646e3d2e67dae5cc0570237580
Malek N <Malek@Necibi.us>
2024-05-20 09:04:30 -0500

    wip: Quick Color alternative for auto-replace current instance

--- scripts/Quick_Color.sh
+++ scripts/Quick_Color.sh
@@ -8,6 +8,11 @@ script_name=$(basename "$BASH_SOURCE")
 # kill all instances of Quick_Color.sh (excluding self)
 2>/dev/null kill $(ps -eo "pid,args" | grep "$script_name" | grep -v -e "grep" -e "$$" -e "$BASHPID" | awk "{ print \$1 }") ;
 
+# TODO: replace above with below, split off into separate script. NEEDS TO ONLY DELETE OLDER INSTANCES
+
+# SCREEN_NAME="Quick_Color"
+# screens=$(screen -ls | grep "${SCREEN_NAME}"'\s*(Detached)$') && pids=$(echo -n "$screens" | cut -d. -f1 | awk '{print $1}' | tr '\n' '|' | sed 's/|$//') && ps -eo ppid,pid,args | grep -E "^($pids)" | awk '{print $2}' | xargs -r kill;
+
 start=$(date +"%s")
 stop=$((start + 10))
 until [[ "$(date +'%s')" -gt "$stop" ]] ; do
@@ -16,3 +21,4 @@ until [[ "$(date +'%s')" -gt "$stop" ]] ; do
 done;
 
 activator send switch-on.com.PS.GrayscaleFS;
+# springcuts -r "Color Current App"
